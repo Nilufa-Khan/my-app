21221bf35184d0724aaf50cc2de4faf11b023c89
Nilufa Khan <khannilufa5@gmail.com>
2023-02-02 00:08:56 +0530

    React basic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+let name = "Nilufa";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+   <nav className="navbar">
+    <li>Home</li>
+    <li>About</li>
+    <li>Contact</li>
+   </nav>
+   <div className="cont">
+    <h1>Hello {name}</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur obcaecati praesentium assumenda hic, laudantium ut voluptas! Reiciendis minus porro tempore ducimus, labore sit consequuntur assumenda, fuga, vel enim maiores voluptate.</p>
+    
+    const element = <h1>Hello, world!</h1>; {/* This funny tag syntax is neither a string nor HTML.
+
+It is called JSX, and it is a syntax extension to JavaScript.  */}
+   </div>
+   <div className="example" >
+    How is React
+    <li className="name">
+      lovely
+    </li>
+   </div>
+   </>
   );
 }
 
