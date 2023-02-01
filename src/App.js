3730ef6be09dcc7f6059da0a0049d90e3738acfe
Nilufa Khan <khannilufa5@gmail.com>
2023-02-02 00:45:03 +0530

    nav

--- src/App.js
+++ src/App.js
@@ -5,27 +5,30 @@ let name = "Nilufa";
 function App() {
   return (
     <>
-   <nav className="navbar">
-    
-    <li>Home</li>
-    <li>About</li>
-    <li>Contact</li>
-   </nav>
-   <div className="cont">
-    <h1>Hello {name}</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur obcaecati praesentium assumenda hic, laudantium ut voluptas! Reiciendis minus porro tempore ducimus, labore sit consequuntur assumenda, fuga, vel enim maiores voluptate.</p>
-    
-    const element = <h1>Hello, world!</h1>; {/* This funny tag syntax is neither a string nor HTML.
-
-It is called JSX, and it is a syntax extension to JavaScript.  */}
-   </div>
-   <div className="example" >
-    How is React
-    <li className="name">
-      lovely
-    </li>
-   </div>
-   </>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+      <a className="navbar-brand" href="/">TextUtils</a>
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/">About</a>
+        </li>
+      
+      </ul>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+  </>
   );
 }
 
