056a59b224eebe7e2dcec173dbbe36fbd311a6d2
Nilufa Khan <khannilufa5@gmail.com>
2023-02-03 00:06:09 +0530

    added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 export default function Navbar(props) {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
     <div className="container-fluid">
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,7 +2,7 @@ import React,{useState} from 'react'
 
 
 export default function TextForm(props) {
-  const [text,setText] = useState('Enter ....');
+  const [text,setText] = useState('Enter text here');
   // text = "new text"; // Wrong way to change the state
 // setText("new text"); // Correct way to change the state
 
@@ -12,6 +12,18 @@ const handleUpClick = ()=>{
   let newText = text.toUpperCase();
   setText(newText)
   }
+  const handleLowClick =()=>{
+    let lowText = text.toLowerCase();
+    setText(lowText);
+  }
+
+  const handleClearClick =()=>{
+    let lowText ='';
+    setText(lowText);
+  }
+
+
+
   const handleOnChange = (event)=>{
   // console.log("On change");
   
@@ -21,12 +33,24 @@ const handleUpClick = ()=>{
   return (
     <div>
     <h1>{props.title}</h1>
-    <div className="mb-3">
+    <div className="mb-4">
    
-    <textarea className="form-control" id="myBox" value={text} onChange={handleOnChange} rows="6"></textarea>
+    <textarea className="form-control" placeholder='Enter your text' id="myBox" value={text} onChange={handleOnChange} rows="6"></textarea>
     </div>
-    <button type="submit" className="btn btn-primary" onClick={handleUpClick}>Convert To UpperCase</button>
+    <button type="submit" className="btn btn-primary mb-5 mx-1" onClick={handleUpClick}>Convert To UpperCase</button>
+    <button type="submit" className="btn btn-primary mb-5 mx-1" onClick={handleLowClick}>Convert To LowerCase</button>
+    <button type="submit" className="btn btn-primary mb-5 mx-1" onClick={handleClearClick}>Clear</button>
     
+    <div className="container  my-4">
+      <h3>Yor text Summary: </h3>
+      <p>{text.split(" ").length} words and {text.length} characters</p>
+      <p>Average {0.08 * text.split(" ").length} Minutes take time to Read</p>
+    </div>
+    <h3>Text Preview</h3>
+    <div className="card my-4">
+     
+      <p>{text}</p>
+    </div>
     </div>
   
 
